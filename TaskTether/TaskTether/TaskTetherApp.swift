05c4f76a030f13796c26896d4e9cffee7e1d83d6
Hazim Sami (he/him) <1193645+hazim@users.noreply.github.com>
2026-03-10 12:53:32 +0100

    Add .gitignore for Xcode and credentials

---
+++ TaskTether/TaskTether/TaskTetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTetherApp.swift
+//  TaskTether
+//
+//  Created by Hazim Sami on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskTetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
